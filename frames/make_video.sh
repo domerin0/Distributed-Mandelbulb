eedbbf967620e0aa5b2f811084e546ed566428cd
Epp14 <rob_epp@live.com>
2016-03-22 14:04:05 -0400

    Better handling of title frames and audio support, included dependant files

--- frames/make_video.sh
+++ frames/make_video.sh
@@ -1,4 +1,14 @@
-ffmpeg -framerate 30 -loop 1 -i title.bmp -c:v libx264 -t 5 1.mp4
-ffmpeg -framerate 30 -i frame_%04d.bmp -i conc.mp3 -c:v libx264 -c:a copy -shortest 2.mp4
+#create title video
+ffmpeg -framerate 30 -loop 1 -i title.bmp -c:v libx264 -t 3 a.mp4
 
-ffmpeg -i 'concat:1.mp4|2.mp4' -codec copy output.mp4
+#combine video frmaes to gether
+ffmpeg -framerate 30 -i frame_%04d.bmp -c:v libx264 b.mp4
+
+#join title to video
+ffmpeg -f concat -i playlist.txt -c copy no_sound.mp4
+
+#add aduio track, -shortest stops the video when the shortest stream is over (the audio track should be longer than 4min)
+ffmpeg -i conc.mp3 -i no_sound.mp4 -c copy -shortest output.mp4
+
+#clean up
+rm a.mp4 b.mp4 no_sound.mp4
